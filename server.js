af8271a84a80ddc617822afe4ad5da05c224e260
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-02-23 20:42:47 -0500

    added MongoDB to project

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-const express = require('express');
+const express = require('express'); // for api
+const mongoose = require('mongoose'); // for MongoDB
+const bodyParser = require('body-parser'); // to parse incoming requests
+
+// start the app
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
